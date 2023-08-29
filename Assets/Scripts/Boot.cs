e9a42a75b69ecb7c48a622e4861226f15bcff9d3
R1nge <r1ngegamedev@gmail.com>
2023-08-29 12:34:40 +0300

    Scene load example

---
+++ Assets/Scripts/Boot.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using Zenject;
+
+public class Boot : MonoBehaviour
+{
+    private ZenjectSceneLoader _zenjectSceneLoader;
+
+    [Inject]
+    private void Inject(ZenjectSceneLoader zenjectSceneLoader) => _zenjectSceneLoader = zenjectSceneLoader;
+
+    private void Awake() => _zenjectSceneLoader.LoadSceneAsync("SampleScene", LoadSceneMode.Additive);
+}
