43bdce08dcc34a251e8b1dcca011571d6c367578
R1nge <r1ngegamedev@gmail.com>
2023-08-29 10:46:00 +0300

    Spawner example

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -1,6 +1,15 @@
 ﻿using UnityEngine;
+using Zenject;
 
 public class PlayerSpawner : MonoBehaviour
 {
-        
+    [SerializeField] private Player prefab;
+    private DiContainer _diContainer;
+
+    [Inject]
+    private void Inject(DiContainer diContainer) => _diContainer = diContainer;
+
+    private void Awake() => Spawn();
+
+    private void Spawn() => _diContainer.InstantiatePrefabForComponent<Player>(prefab);
 }
